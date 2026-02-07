4a903fc2868767d4c130fe054c4880a2d1f6d5ca
Moocow <moocow.gg@gmail.com>
2026-02-07 12:50:58 -0500

    css changes, and rat move

--- game.js
+++ game.js
@@ -1,7 +1,11 @@
 let round = 1;
 let punch_cnt = 0;
 let punch_cnt_display = document.getElementById("punch_cnt");
+
 let rat_life = 3;
+let rat = document.getElementById("rat");
+let rat_move = ['sprites/rat/rat_move1.png', 'sprites/rat/rat_move2.png', 'sprites/rat/rat_move3.png', 'sprites/rat/rat_move4.png'];
+let rat_currMove = 0;
 
 let yes_btn = document.getElementById("yes_btn");
 let no_btn = document.getElementById("no_btn");
@@ -13,13 +17,23 @@ let punch_btn = document.getElementById("punch_btn");
 let timer = document.getElementById("timer");
 
 yes_btn.addEventListener('click', function() {
+    if (rat_life>0){
+        ratMove();
+    }
+    else{
+        window.location.href = 'gift.html';
+    }
+});
+no_btn.addEventListener('click', function(){
     question_page.style.display = 'none';
     gift.style.display = 'block';
-});
+})
 
-if (rat_life>0){
-    yes_btn.style.display = 'none';
-}
-else{
-    window.location.href = 'gift.html';
+function ratMove(){
+    rat.style.display = 'block';
+    
+    let frameInterval = setInterval(function() {
+        rat_currMove = (rat_currMove + 1) % 4;
+        rat.src = rat_move[rat_currMove];
+    }, 100);
 }
